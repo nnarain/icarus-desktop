c691e40e4f2df3b2719f9728360887432a6f57a1
Natesh Narain <nnaraindev@gmail.com>
2023-02-12 10:57:10 -0500

    Simple bevy setup

--- icarus-desktop/src/main.rs
+++ icarus-desktop/src/main.rs
@@ -1,3 +1,23 @@
+//
+// main.rs
+//
+// @author Natesh Narain <nnaraindev@gmail.com>
+// @date Feb 12 2023
+//
+
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+
+fn ui_system(mut ctx: ResMut<EguiContext>) {
+    egui::Window::new("Icarus Desktop").show(ctx.ctx_mut(), |ui| {
+        ui.label("This is text");
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        .add_system(ui_system)
+        .run();
 }
